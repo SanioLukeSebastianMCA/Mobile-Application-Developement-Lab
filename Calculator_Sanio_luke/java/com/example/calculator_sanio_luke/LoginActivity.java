170fc5d18cdbb7ff6866ba10dbcade08ec1886ed
Sanio Luke Sebastian <93911177+SanioLukeSebastianMCA@users.noreply.github.com>
2022-09-20 13:06:43 +0530

    Add files via upload

---
+++ Calculator_Sanio_luke/java/com/example/calculator_sanio_luke/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.calculator_sanio_luke;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout username, password;
+    private Button login_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username= findViewById(R.id.username);
+        password= findViewById(R.id.password);
+        login_btn= findViewById(R.id.login_btn);
+
+        login_btn.setOnClickListener(v->{
+            String username_txt= username.getEditText().getText().toString();
+            String password_txt= password.getEditText().getText().toString();
+
+            if(username_txt.length() <= 0){
+                username.setError("Please enter a username !!");
+                username.requestFocus();
+            }
+            else if(password_txt.length() <= 0){
+                password.setError("Please enter a password !!");
+                password.requestFocus();
+            }
+            else{
+                username.setError(null);
+                password.setError(null);
+                Toast.makeText(this, "Login Successful.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
